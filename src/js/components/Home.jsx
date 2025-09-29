bcac6e8e24c00f05533bac7f597cdd8fc7045795
Naisthecreator <42411601+naisthecreator@users.noreply.github.com>
2025-09-29 07:39:39 +0000

    Added first page

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<div className="container-fluid bg-dark py-3">
+			<div className="row">
+				<div className="col-6 d-flex align-items-center py-1">
+					<h3 className="text-white">Start Bootstrap</h3>
+				</div>
+				<div className="col-6 d-flex justify-content-end">
+					<ul className="list-group list-group-horizontal list-group-flush">
+						<li className="list-group-item text-white">Home</li>
+						<li className="list-group-item text-white">About</li>
+						<li className="list-group-item text-white">Services</li>
+						<li className="list-group-item text-white">Contact</li>
+					</ul>
+				</div>
+			</div>
 		</div>
+		
+		<div className="container bg-light p-4 mt-4 rounded-4">
+			<h1 className="pt-5" style = {{fontSize: "4rem"}}>A Warm Welcome!</h1>
+			<p className="fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet sint dicta explicabo, nobis distinctio iure quod nam molestias ea, asperiores quibusdam reiciendis molestiae eaque ipsum dignissimos inventore vero, ullam sit.</p>
+			<button className="btn btn-primary mb-4">Call to action!</button>
+		</div>
+		</>
 	);
+	
 };
 
 export default Home;
